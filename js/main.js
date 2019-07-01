a6a1f6f905c1da4b6098f48bf3d79ee91c4ec375
mirenguerra <miren.guerra.sanz@gmail.com>
2019-07-01 19:46:13 +0200

    Add style to title and footer

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
-'use strict';
+"use strict";
 
 // Add functionality to btns
-const menuBtn = document.querySelector('.nav-trigger');
-const closeBtn = document.querySelector('.nav-close');
-const navMenu = document.querySelector('.nav');
+const menuBtn = document.querySelector(".nav-trigger");
+const closeBtn = document.querySelector(".nav-close");
+const navMenu = document.querySelector(".nav");
 
 function toggleMenu() {
-    navMenu.classList.toggle('menu-hidden');
+  navMenu.classList.toggle("menu-hidden");
 }
 
-menuBtn.addEventListener('click', toggleMenu);
-closeBtn.addEventListener('click', toggleMenu);
+menuBtn.addEventListener("click", toggleMenu);
+closeBtn.addEventListener("click", toggleMenu);
 
 // Add img to menuBtn
-menuBtn.innerHTML ='';
+menuBtn.innerHTML = "";
 const menuImg = document.createElement("img");
 menuImg.setAttribute("src", "images/ico-menu.svg");
 menuBtn.appendChild(menuImg);
